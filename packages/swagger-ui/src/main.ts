4e11f213eba206dd3787f03e694074b51c27cb22
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-06-20 23:08:39 +0200

    fix: symlink openapi document into public.

--- packages/swagger-ui/src/main.ts
+++ packages/swagger-ui/src/main.ts
@@ -1,9 +1,7 @@
 import SwaggerUI from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 
-import documentUrl from "@scouterna/scoutnet-openapi/scoutnet.yaml?url";
-
 SwaggerUI({
   dom_id: "#app",
-  url: documentUrl,
+  url: "./scoutnet.yaml",
 });
